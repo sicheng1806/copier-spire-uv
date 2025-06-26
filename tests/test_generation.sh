49c34187a33317ec809a3833c0bd7d999878d68c
sicheng1806 <sicheng1806@outlook.com>
2025-06-26 13:40:58 +0800

    feat(project): 添加sphinx工具

--- tests/test_generation.sh
+++ tests/test_generation.sh
@@ -3,13 +3,27 @@ set -eu
 export TESTING=true
 
 output=tests/tmp 
-template=.
+
+echo "///////////////////////////////////////////"
+echo "             TAGGING TEMPLATE COPY"
+echo "///////////////////////////////////////////"
+echo
+template=$(mktemp -d)
+cp -rf . "${template}"
+(
+  cd "${template}" || exit 1
+  git add . -A || true
+  git commit -m "test" || true
+  git tag 99.99.99 -m "test"
+)
+echo "Template copy located at ${template}"
 
 echo
 echo "///////////////////////////////////////////"
 echo "             GENERATING PROJECT"
 echo "///////////////////////////////////////////"
 echo
+rm -rf "${output}"
 copier copy -f --trust "${template}" "${output}" \
     -d project_name="Template Testing" \
     -d project_description="A test project" \
@@ -21,4 +35,9 @@ echo "             PROJECT RESULT"
 echo "///////////////////////////////////////////"
 echo
 cd "${output}"
+echo 
+echo "             COPIER-ANSWERS"
+cat .copier-answers.yml
+echo
+echo "             PROJECT STRUCTURE"
 tree -a
