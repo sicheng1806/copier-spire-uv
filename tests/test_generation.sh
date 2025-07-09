28279cd4aeca8a223d9356e34041b3f32ccebb30
sicheng1806 <sicheng1806@outlook.com>
2025-07-09 15:32:03 +0800

    fix(test): fix the test script.

--- tests/test_generation.sh
+++ tests/test_generation.sh
@@ -16,6 +16,7 @@ cp -rf . "${template}"
   git commit -m "test" || true
   git tag 99.99.99 -m "test"
 )
+
 echo "Template copy located at ${template}"
 
 echo
@@ -28,12 +29,7 @@ copier copy -f --trust "${template}" "${output}" \
     -d project_name="Template Testing" \
     -d project_description="A test project" \
     -d author="sicheng" \
-
-echo
-echo "///////////////////////////////////////////"
-echo "             PROJECT RESULT"
-echo "///////////////////////////////////////////"
-echo
+    > /dev/null 2>&1
 cd "${output}"
 echo 
 echo "             COPIER-ANSWERS"
@@ -41,3 +37,36 @@ cat .copier-answers.yml
 echo
 echo "             PROJECT STRUCTURE"
 tree -a
+
+echo
+echo "///////////////////////////////////////////"
+echo "             TESTING PROJECT"
+echo "///////////////////////////////////////////"
+echo
+git init .
+git add -A .
+git commit -m "feat: Initial commit"
+git tag v9.9.999 -m "test"
+echo
+echo ">>> Setting up Python environments"
+uv sync > /dev/null 2>&1
+
+
+
+echo
+echo "///////////////////////////////////////////"
+echo "             UPDATING PROJECT"
+echo "///////////////////////////////////////////"
+echo
+echo uv.lock >> .gitignore
+git add .
+git commit -m "chore: commit before copier update"
+copier update -f --trust > /dev/null 2>&1
+
+echo
+echo "///////////////////////////////////////////"
+echo "         CLEANUP TEMPLATE"
+echo "///////////////////////////////////////////"
+echo
+echo ">>> Removing ${template}"
+rm -rf "${template}"
